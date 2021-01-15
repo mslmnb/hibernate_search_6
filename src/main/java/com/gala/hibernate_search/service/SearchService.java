cce827b2ab6d8e989cd41d95b431618224d0fdc5
Galiya Mussulmanbekova <mslmn@rambler.ru>
2021-01-15 14:00:18 +0600

    Searching by product's name and status by predicate

--- src/main/java/com/gala/hibernate_search/service/SearchService.java
+++ src/main/java/com/gala/hibernate_search/service/SearchService.java
@@ -2,15 +2,25 @@ package com.gala.hibernate_search.service;
 
 import com.gala.hibernate_search.model.Book;
 import com.gala.hibernate_search.model.product.Product;
+import com.gala.hibernate_search.model.product.ProductStatus;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.search.engine.search.common.BooleanOperator;
+import org.hibernate.search.engine.search.predicate.SearchPredicate;
+import org.hibernate.search.engine.search.predicate.dsl.BooleanPredicateClausesStep;
+import org.hibernate.search.engine.search.predicate.dsl.PredicateFinalStep;
+import org.hibernate.search.engine.search.predicate.dsl.SearchPredicateFactory;
+import org.hibernate.search.engine.search.predicate.dsl.SimpleQueryStringPredicateFieldStep;
 import org.hibernate.search.engine.search.query.SearchResult;
 import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.scope.SearchScope;
 import org.hibernate.search.mapper.orm.session.SearchSession;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -35,13 +45,51 @@ public class SearchService {
         return result.hits();
     }
 
-    public List<Product> getProductsByName(String name) {
-        SearchSession searchSession = Search.session(entityManager);
-        SearchResult<Product> result = searchSession.search(Product.class)
-                .where(f -> f.match()
-                        .fields("name.ru", "name.kk", "name.qq")
-                        .matching(name)
-                ).fetch(20);
-        return result.hits();
+    public List<Product> getProductsByName(String searchPhrase) {
+/*  // поиск через лямбда
+        SearchSession searchSession = Search.session( entityManager );
+        List<Product> hits = searchSession.search( Product.class )
+                .where( f -> {
+                    BooleanPredicateClausesStep<?> step = f.bool()
+                            .minimumShouldMatchNumber(1)
+                            .should(f.simpleQueryString().field("name.ru").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                            .should(f.simpleQueryString().field("name.kk").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                            .should(f.simpleQueryString().field("name.qq").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                            .must(f.match().field("status").matching(ProductStatus.ACTIVE));
+                    return step;
+                        }
+                )
+                .fetchHits( 20 );
+        return hits;
+
+ */
+
+//     поиск через предикаты
+        SearchSession searchSession = Search.session( entityManager );
+        SearchScope<Product> scope = searchSession.scope( Product.class );
+        SearchPredicateFactory factory = scope.predicate();
+
+        SearchPredicate predicate1 = factory.bool()
+                .minimumShouldMatchNumber(1)
+                .should(factory.simpleQueryString().field("name.ru").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                .should(factory.simpleQueryString().field("name.kk").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                .should(factory.simpleQueryString().field("name.qq").matching(searchPhrase).defaultOperator(BooleanOperator.AND))
+                .toPredicate();
+
+        SearchPredicate predicate2 = factory.match()
+                .field("status")
+                .matching(ProductStatus.ACTIVE)
+                .toPredicate();
+
+        BooleanPredicateClausesStep<?> booleanJunction = factory.bool();
+        booleanJunction.must( predicate1 );
+        booleanJunction.must( predicate2 );
+
+        SearchPredicate boolPredicate = booleanJunction.toPredicate();
+
+        List<Product> result = searchSession.search( scope )
+                .where( boolPredicate )
+                .fetchHits( 20 );
+        return result;
     }
 }
